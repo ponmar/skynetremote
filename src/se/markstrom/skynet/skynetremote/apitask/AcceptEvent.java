555a602013e94682195476913aaa743a02add6cd
Pontus Markström <pontus.markstrom@gmail.com>
2015-09-05 11:29:47 +0200

    Added tasks for accepting events

---
+++ src/se/markstrom/skynet/skynetremote/apitask/AcceptEvent.java
@@ -0,0 +1,22 @@
+package se.markstrom.skynet.skynetremote.apitask;
+
+import se.markstrom.skynet.api.SkynetAPI;
+import se.markstrom.skynet.api.SkynetAPI.SkynetAPIError;
+import se.markstrom.skynet.api.SkynetAPIClientError;
+import se.markstrom.skynet.skynetremote.GUI;
+
+public class AcceptEvent implements ApiTask {
+
+	private long eventId;
+	
+	public AcceptEvent(long eventId) {
+		this.eventId = eventId;
+	}
+	
+	@Override
+	public void run(ApiThread apiThread, SkynetAPI api, GUI gui) throws SkynetAPIClientError, SkynetAPIError {
+		if (api != null) {
+			api.acceptEvent(eventId);
+		}
+	}
+}
---
+++ src/se/markstrom/skynet/skynetremote/apitask/AcceptEvents.java
@@ -0,0 +1,34 @@
+package se.markstrom.skynet.skynetremote.apitask;
+
+import se.markstrom.skynet.api.SkynetAPI;
+import se.markstrom.skynet.api.SkynetAPIClientError;
+import se.markstrom.skynet.api.SkynetAPI.SkynetAPIError;
+import se.markstrom.skynet.skynetremote.GUI;
+
+public class AcceptEvents implements ApiTask {
+	
+	public enum EventGroup { ALL_MINOR, ALL_MAJOR, ALL };
+	
+	private EventGroup eventGroup;
+	
+	public AcceptEvents(EventGroup eventGroup) {
+		this.eventGroup = eventGroup;
+	}
+
+	@Override
+	public void run(ApiThread apiThread, SkynetAPI api, GUI gui) throws SkynetAPIClientError, SkynetAPIError {
+		if (api != null) {
+			switch (eventGroup) {
+			case ALL:
+				api.acceptEvent(-1);
+				break;
+			case ALL_MINOR:
+				api.acceptEvent(-2);
+				break;
+			case ALL_MAJOR:
+				api.acceptEvent(-3);
+				break;
+			}
+		}
+	}
+}
